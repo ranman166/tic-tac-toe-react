5e4796eab3f342d8d561da9585f5279f58017ce4
Randy Amundson <randy@Randys-MacBook-Pro.local>
2025-01-07 22:33:23 -0600

    updated winner statement

--- src/Tic.tsx
+++ src/Tic.tsx
@@ -7,6 +7,7 @@ interface CellProps {
   player: number,
   setCurrentPlayer: React.Dispatch<React.SetStateAction<number>>
   position: number[] // is there a tuple eqvalent or should i just create a specific interface for this?
+  setWinner: React.Dispatch<React.SetStateAction<number>> 
 }
 
 const trackerboard = [[0,0,0],
@@ -75,28 +76,33 @@ function Cell(CellProps: CellProps){
 
     console.log("handleClick")
 
-    if (CellProps.cellState === "X"){
-      // currently player1
-      trackerboard[CellProps.position[0]][CellProps.position[1]] = 1
-      CellProps.setCellState("O")
-      //note this sets it before it's retured
-      
-    }
-    else if (CellProps.cellState === "O"){
-        // currently player2
+    if (CellProps.cellState === "_"){
+      // can be clicked
+      let win = 0
+
+      if (CellProps.player === 1) {
         trackerboard[CellProps.position[0]][CellProps.position[1]] = 1
         CellProps.setCellState("X")
-    } 
+        win = check_for_win()
+        // next player's turn
+        CellProps.setCurrentPlayer(2)
+      }
+      if (CellProps.player === 2){
+        trackerboard[CellProps.position[0]][CellProps.position[1]] = 2
+        CellProps.setCellState("O")
+        win = check_for_win()
+        // next player's turn
+        CellProps.setCurrentPlayer(1)
 
-
-    let win = check_for_win()
-    if (win === 1) {
-      // player 1 wins
-      console.log("player 1 wins")
-    }
-    else if (win === 2) {
-      // player 2 wins
-      console.log("player 2 wins")
+      }
+      if (win === 1) {
+        // player 1 wins
+        CellProps.setWinner(1)
+      }
+      else if (win === 2) {
+        // player 2 wins
+        CellProps.setWinner(2)
+      }
     }
   }
 
@@ -119,26 +125,35 @@ function TicTac({})  {
   const [cell21, setCell21] = useState("_")
   const [cell22, setCell22] = useState("_")
   const [player, setCurrentPlayer] = useState(1)
+  const [winner, setWinner] = useState(0)
+
+
+  if (winner !== 0){
+    return (<div>
+      <div> Player {winner} won </div>
+    </div>
+    )
+  }
 
 
   return(
   <div >
     <div className="row">
-      <Cell cellState={cell00} setCellState={setCell00} player={player} setCurrentPlayer={setCurrentPlayer} position={[0,0]}/>
-      <Cell cellState={cell01} setCellState={setCell01} player={player} setCurrentPlayer={setCurrentPlayer} position={[0,1]}/>
-      <Cell cellState={cell02} setCellState={setCell02} player={player} setCurrentPlayer={setCurrentPlayer} position={[0,2]}/>
+      <Cell cellState={cell00} setCellState={setCell00} player={player} setCurrentPlayer={setCurrentPlayer} position={[0,0]} setWinner={setWinner}/>
+      <Cell cellState={cell01} setCellState={setCell01} player={player} setCurrentPlayer={setCurrentPlayer} position={[0,1]} setWinner={setWinner}/>
+      <Cell cellState={cell02} setCellState={setCell02} player={player} setCurrentPlayer={setCurrentPlayer} position={[0,2]} setWinner={setWinner}/>
 
     </div>
     <div className="row">
-      <Cell cellState={cell10} setCellState={setCell10} player={player} setCurrentPlayer={setCurrentPlayer} position={[1,0]}/>
-      <Cell cellState={cell11} setCellState={setCell11} player={player} setCurrentPlayer={setCurrentPlayer} position={[1,1]}/>
-      <Cell cellState={cell12} setCellState={setCell12} player={player} setCurrentPlayer={setCurrentPlayer} position={[1,2]}/>
+      <Cell cellState={cell10} setCellState={setCell10} player={player} setCurrentPlayer={setCurrentPlayer} position={[1,0]} setWinner={setWinner}/>
+      <Cell cellState={cell11} setCellState={setCell11} player={player} setCurrentPlayer={setCurrentPlayer} position={[1,1]} setWinner={setWinner}/>
+      <Cell cellState={cell12} setCellState={setCell12} player={player} setCurrentPlayer={setCurrentPlayer} position={[1,2]} setWinner={setWinner}/>
 
     </div>
     <div className="row">
-      <Cell cellState={cell20} setCellState={setCell20} player={player} setCurrentPlayer={setCurrentPlayer} position={[2,0]}/>
-      <Cell cellState={cell21} setCellState={setCell21} player={player} setCurrentPlayer={setCurrentPlayer} position={[2,1]}/>
-      <Cell cellState={cell22} setCellState={setCell22} player={player} setCurrentPlayer={setCurrentPlayer} position={[2,2]}/>
+      <Cell cellState={cell20} setCellState={setCell20} player={player} setCurrentPlayer={setCurrentPlayer} position={[2,0]} setWinner={setWinner}/>
+      <Cell cellState={cell21} setCellState={setCell21} player={player} setCurrentPlayer={setCurrentPlayer} position={[2,1]} setWinner={setWinner}/>
+      <Cell cellState={cell22} setCellState={setCell22} player={player} setCurrentPlayer={setCurrentPlayer} position={[2,2]} setWinner={setWinner}/>
     </div>
 
   </div>
